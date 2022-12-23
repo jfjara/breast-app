5fb425e07a73388de95e483ac0b77ef40f8245e1
jfjara <juanfranciscojara@gmail.com>
2022-12-23 23:55:24 +0100

    Create rating
    Add endpoint in controller, usecase, config, and mock repository.

--- breast-app-rating-service/src/main/java/com/breastapp/breastappratingservice/api/controller/RatingController.java
+++ breast-app-rating-service/src/main/java/com/breastapp/breastappratingservice/api/controller/RatingController.java
@@ -1,18 +1,22 @@
 package com.breastapp.breastappratingservice.api.controller;
 
 import com.breastapp.breastappratingservice.api.mapper.PlaceRatingGlobalMapper;
+import com.breastapp.breastappratingservice.api.mapper.PlaceRatingMapper;
 import com.breastapp.breastappratingservice.api.mapper.PlaceRatingResumeMapper;
+import com.breastapp.breastappratingservice.api.model.PlaceRating;
 import com.breastapp.breastappratingservice.api.model.PlaceRatingGlobal;
 import com.breastapp.breastappratingservice.api.model.PlaceRatingResume;
+import com.breastapp.breastappratingservice.application.usecase.interfaces.CreateRatingForPlaceUseCase;
 import com.breastapp.breastappratingservice.application.usecase.interfaces.GetCompleteRatingByPlaceIdUseCase;
 import com.breastapp.breastappratingservice.application.usecase.interfaces.GetResumeRatingByPlaceIdUseCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -29,12 +33,19 @@ public class RatingController {
     private GetCompleteRatingByPlaceIdUseCase getCompleteRatingByPlaceIdUseCase;
 
     @Autowired
+    @Qualifier("CreateRatingForPlaceUseCaseImpl")
+    private CreateRatingForPlaceUseCase createRatingForPlaceUseCase;
+
+    @Autowired
     private PlaceRatingGlobalMapper placeRatingGlobalMapper;
 
     @Autowired
     private PlaceRatingResumeMapper placeRatingResumeMapper;
 
-    @GetMapping("/rating/resume/{id}")
+    @Autowired
+    private PlaceRatingMapper placeRatingMapper;
+
+    @GetMapping(value = "/rating/resume/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<PlaceRatingResume> getPlaceResume(@PathVariable final String id) {
         logger.info("GET /rating/resume/{}", id);
         return Mono.just(
@@ -44,7 +55,7 @@ public class RatingController {
         );
     }
 
-    @GetMapping("/rating/{id}")
+    @GetMapping(value = "/rating/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<PlaceRatingGlobal> getCompleteRating(@PathVariable final String id) {
         logger.info("GET /rating/{}", id);
         return Mono.just(
@@ -52,6 +63,15 @@ public class RatingController {
                         .map(r -> placeRatingGlobalMapper.toApiModel(r))
                         .orElse(null)
         );
+    }
+
+    @PostMapping(value = "/rating", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<HttpStatus>> createRating(@RequestBody final PlaceRating placeRating) {
+        logger.info("POST /rating {}", placeRating.toString());
+        return Mono.just(createRatingForPlaceUseCase.execute(placeRatingMapper.toDtoModel(placeRating)) ?
+                new ResponseEntity<>(HttpStatus.CREATED) :
+                new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+
     }
 
 }
